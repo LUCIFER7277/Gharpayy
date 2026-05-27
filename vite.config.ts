57b8a580cad5ca44e32997f772e82310d066d5ef
Amit <amitantu7277@gmail.com>
2026-05-27 16:42:34 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig();
-
-
-
+export default defineConfig({
+  cloudflare: process.env.VERCEL ? false : undefined,
+});
